e9f0ece62bee631809fac082d731aa55123c007f
ganeshbabu1994 <vishnuganesh1994@gmail.com>
2025-05-30 17:45:55 +0530

    controller seperated for each GET POST api's to sends response back to fetch client call on service side

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,8 +1,37 @@
 
-// const MainController=()=> {
-//     console.log("CONTROLLER")
-// }
 
-// module.exports = {
-//  MainController
-//   }; 
+const GetController = () => {
+    const response = () => {
+        api.getMethod(function (key, value, res, callback) {
+            // controller to interconnect client base url and backend api's 
+            console.log("GET controller")
+            res.send({ json: res })
+        })
+    }
+}
+
+const PostController = () => {
+    const response = () => {
+        api.getMethod(function (key, value, res, callback) {
+            // controller to interconnect client base url and backend api's 
+            console.log("Post controller")
+            res.send({ json: res })
+        })
+    }
+}
+
+const GetDepartmentList = () => {
+    const response = () => {
+        api.getMethod(function (key, value, res, callback) {
+            // controller to interconnect client base url and backend api's 
+            console.log("Department controller")
+            res.send({ json: res })
+        })
+    }
+}
+
+module.exports = {
+    GetController,
+    PostController,
+    GetDepartmentList
+}; 
